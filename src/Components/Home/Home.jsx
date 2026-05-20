513988451728b264ccb581071af8ca681ce31ba5
MAI HEKAIL <heikalmai402@gmail.com>
2026-05-20 14:19:22 +0300

    mai

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, odio. Repellat excepturi culpa similique vero voluptates adipisci veritatis, quae ipsam modi sunt aliquam corporis laborum magni asperiores natus, impedit saepe!</p>
+   
   )
 }
